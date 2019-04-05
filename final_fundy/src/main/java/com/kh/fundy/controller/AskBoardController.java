81bc8d20c9c3269a5b1ee203e5ad871054f4976d
Shinjaem063 <jester0603@gmail.com>
2019-04-05 12:20:19 +0900

    게시판 페이징처리 80프로 완성중-잼잼

--- final_fundy/src/main/java/com/kh/fundy/controller/AskBoardController.java
+++ final_fundy/src/main/java/com/kh/fundy/controller/AskBoardController.java
@@ -1,9 +1,15 @@
 package com.kh.fundy.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.fundy.common.PageBarFactory;
+import com.kh.fundy.model.vo.AskBoard;
 import com.kh.fundy.service.AskBoardService;
 
 
@@ -21,8 +27,20 @@ public class AskBoardController {
 	}
 	
 	@RequestMapping("/askBoardMain.do")
-	public String askBoardMain()
+	public ModelAndView askBoardMain(@RequestParam(value="cPage",required=false, defaultValue="1")int cPage)
 	{
-		return "/askBoard/askBoardMain";
+		int numPerPage=10;
+		ModelAndView mv=new ModelAndView();
+		
+		List<AskBoard> list=service.selectList(cPage,numPerPage);
+		int totalList=service.selectCount();
+		
+		mv.addObject("list",list);
+		mv.addObject("totalList",totalList);
+		mv.addObject("pageBar",PageBarFactory.getPageBar(totalList,cPage,numPerPage,"/spring/askBoard/askBoardMain"));
+		mv.setViewName("askBoard/askBoardMain");
+		
+		
+		return mv;
 	}
 }
--- final_fundy/src/main/java/com/kh/fundy/service/AskBoardServiceImpl.java
+++ final_fundy/src/main/java/com/kh/fundy/service/AskBoardServiceImpl.java
@@ -1,8 +1,31 @@
 package com.kh.fundy.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.fundy.dao.AskBoardDao;
+import com.kh.fundy.model.vo.AskBoard;
 
 @Service
 public class AskBoardServiceImpl implements AskBoardService {
 
+	@Autowired
+	private AskBoardDao dao;
+	
+	@Override
+	public List<AskBoard> selectList(int cPage, int numPerPage) {
+		
+		return dao.selectList(cPage,numPerPage);
+	}
+
+	@Override
+	public int selectCount() {
+		
+		return dao.selectCount();
+	}
+
+	
+	
 }
