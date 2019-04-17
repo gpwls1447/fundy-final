d31fa21046c160e2bf2dd5923a5d7febba7feb2e
kkjhj <fsds12@naver.com>
2019-04-17 13:10:26 +0900

    톰캣구동시 빈못찾는거해결 및 업데이트문 10분마다 실행되도록 설정변경

--- final_fundy/src/main/java/com/kh/fundy/common/AutoUpdateProjectDDay.java
+++ final_fundy/src/main/java/com/kh/fundy/common/AutoUpdateProjectDDay.java
@@ -1,10 +1,15 @@
 package com.kh.fundy.common;
 
+import java.util.concurrent.Executors;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.stereotype.Repository;
 
 @Configuration
@@ -14,8 +19,21 @@ public class AutoUpdateProjectDDay {
 	@Autowired
 	SqlSessionTemplate session;
 	
-	@Scheduled(cron="* /10 * * * *")	//테스트로 10초마다, 401번 프로젝트 업데이트문 실행
-	public void autoUpdate() {
-		session.update("autoUpdate.projectEndUpdate");
-	}
+	@Bean
+    public MyBean myBean () {
+        return new MyBean();
+    }
+
+    @Bean
+    public TaskScheduler taskExecutor () {
+        return new ConcurrentTaskScheduler(
+                  Executors.newScheduledThreadPool(3));
+    }
+    
+    public class MyBean {
+    	@Scheduled(cron="* 0/10 * * * *")	//10분마다 마감시간지난 프로젝트 상태코드 변경하도록 설정
+    	public void autoUpdate() {
+    		session.update("autoUpdate.projectEndUpdate");
+    	}
+    }
 }
